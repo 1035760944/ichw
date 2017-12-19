fcae00a36f2e7558deeed17c21d21bc8fc78b00d
1035760944 <1035760944@qq.com>
2017-12-19 23:54:08 +0800

    pyassign3

---
+++ pyassign3/wcount.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+"""wcount.py: count words from an Internet file.
+
+__author__ = "Zhangsan"
+__pkuid__  = "1600012345"
+__email__  = "zhangsan@pku.edu.cn"
+"""
+
+import sys
+from urllib.request import urlopen
+import re
+
+
+def wcount(lines, topn=10):
+    """count words from lines of text string, then sort by their counts
+    in reverse order, output the topn (word count), each in one line. 
+    """
+    with open(file_path) as file:
+        lines=file.read()
+        words=re.findall(r'[a-zA-Z]+',lines)
+        count=len(words)
+    return count
+    
+    pass
+
+if __name__ == '__main__':
+
+    if  len(sys.argv) == 1:
+        print('Usage: {} url [topn]'.format(sys.argv[0]))
+        print('  url: URL of the txt file to analyze ')
+        print('  topn: how many (words count) to output. If not given, will output top 10 words')
+        sys.exit(1)
+
+    try:
+        topn = 10
+        if len(sys.argv) == 3:
+            topn = int(sys.argv[2])
+    except ValueError:
+        print('{} is not a valid topn int number'.format(sys.argv[2]))
+        sys.exit(1)
+
+    try:
+        with urlopen(sys.argv[1]) as f:
+            contents = f.read()
+            lines   = contents.decode()
+            wcount(lines, topn)
+    except Exception as err:
+        print(err)
+        sys.exit(1)
